d4557cc640e847677d91356361dab90913546044
OceanAnalystDev <djrichard715@gmail.com>
2026-05-12 13:11:19 -0500

    Release 0.0.6

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 APP_NAME = "Oceaneering Survey Technical Tools"
-APP_VERSION = "0.0.5"
+APP_VERSION = "0.0.6"
 RELEASE_DATE = "2026-05-12"
 DOWNLOAD_URL = "https://raw.githubusercontent.com/OceanAnalystDev/Oceaneering_Survey_Technical_Tools_Releases/main/OSTT.exe"
